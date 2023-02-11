9a19896e03be8352621a3489b209e0071b406305
Igor <jasinski1998@gmail.com>
2023-02-11 19:36:54 +0300

    task_54

---
+++ task_54/Program.cs
@@ -0,0 +1,122 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.Clear();
+
+// void CreateInArray(int[,] array, int line, int column, int minValue, int maxValeu)
+// {
+//     int[,] result = new int[line, column];
+//     for (int i = 0; i < line; i++)
+//     {
+//         for (int j = 0; j < column; j++)
+//         {
+//             result[i, j] = new Random().Next(minValue, maxValeu);
+//         }
+//     }
+// }
+
+// void PrintArray(int[,] inArray)
+// {
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Console.Write($"{inArray[i, j]}\t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void OrderArrayLines(int[,] array)
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       for (int k = 0; k < array.GetLength(1) - 1; k++)
+//       {
+//         if (array[i, k] < array[i, k + 1])
+//         {
+//           int temp = array[i, k + 1];
+//           array[i, k + 1] = array[i, k];
+//           array[i, k] = temp;
+//         }
+//         Console.WriteLine(array);
+//       }
+//     }
+//   }
+// }
+
+// Console.Write("Введите кол-во строк ");
+// int line = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите кол-во столбцов ");
+// int column = int.Parse(Console.ReadLine()!);
+
+// int[,] array = CreateInArray(line, column, 0, 10);
+// PrintArray(array);
+// OrderArrayLines(array);
+
+int[,] CreateArray(int line, int column, int minValue, int maxValeu)
+{
+    int[,] result = new int[line, column];
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValeu);
+        }
+    }
+    return result;
+}
+
+void WriteArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
+void OrderArrayLines(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+Console.Clear();
+Console.WriteLine("Введите кол-во строк: ");
+int line = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+int[,] result = CreateArray(line, column, 0, 10);
+WriteArray(result);
+
+Console.WriteLine($"\n Отсортированный массив: ");
+OrderArrayLines(result);
+WriteArray(result);
